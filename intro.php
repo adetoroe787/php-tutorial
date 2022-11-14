7fe62c98f26ad6653c61b8453990dfff0f74263d
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-11-14 09:27:37 -0500

    php basic

---
+++ intro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    /**** 
+    Author: Ee
+    email: support@
+     *****/
+    $title = "Street";
+    echo "<h1>hello $title </h1>";
+    echo "<hr >";
+    # echo "<p>hello php</p>";
+    // print "<p>This is a PHP example.</p>";
+    ?>
+    <h1>Testing</h1>
+    <?php
+    $heavyweight = "Lennox Lewis";
+    $lightweight = "Floyd Mayweather";
+    echo $heavyweight, " and ", $lightweight, " are great fighters.";
+    ?>
+</body>
+
+</html>
